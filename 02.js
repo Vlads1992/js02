c1676e4885688ee25cce6270b92aca4c5e1079bf
Vlad <vladyslavmyronov@gmail.com>
2019-11-28 10:49:35 +0200

    js module 02, just started

---
+++ 02.js
@@ -0,0 +1,55 @@
+// Запросить у пользователя число от 0 до 9 и вывести ему спецсимвол, 
+// который расположен на этой клавише (1–!, 2–@, 3–# и т. д).
+
+let number = prompt("Пожалуйста введите число от 0 до 9"); 
+
+do  {
+    number = +prompt("Введите диапазон от 0 до 9");
+} while (number < 0);
+
+do  {
+    number = +prompt("Введите диапазон от 0 до 9");
+} while (number > 9);
+
+if (number == 0) {
+    prompt (")");
+}
+
+if (number == 1) {
+    prompt ("!");
+}
+
+if (number == 2) {
+    prompt ("@");
+}
+
+if (number == 3) {
+    prompt ("#");
+}
+
+if (number == 4) {
+    prompt ("$");
+}
+
+if (number == 5) {
+    prompt ("%");
+}
+
+if (number == 6) {
+    prompt ("^");
+}
+
+if (number == 7) {
+    prompt ("&");
+}
+
+if (number == 8) {
+    prompt ("*");
+}
+
+if (number == 9) {
+    prompt ("(");
+}
+
+
+
