9125d04ed5be9073d02625983ab9544a8e9587d4
Vlad <vladyslavmyronov@gmail.com>
2020-07-10 02:45:10 +0300

    js2 complete

--- 03.js
+++ 03.js
@@ -1,2 +1,21 @@
 // Запросить у пользователя трехзначное число и проверить, есть ли в нем одинаковые цифры.
 
+let hw_02_3 = document.getElementById("hw_02_3");
+
+hw_02_3.onclick = function () {
+
+let number, first, second, third
+
+number = prompt("Enter a 3 digit number")
+first = number.slice(0,1);
+second = number.slice(1,2);
+third = number.slice(2,3);
+
+if (first == second || first == third || third == second) {
+    alert(`The number has same digits`)
+} else {
+    alert(`The number doesn't have same digits`)
+}
+
+return false;
+}
--- 05.js
+++ 05.js
@@ -1,2 +1,23 @@
 // Запросить у пользователя пятиразрядное число и определить, является ли оно палиндромом.
 
+let hw_02_5 = document.getElementById("hw_02_5");
+
+hw_02_5.onclick = function () {
+
+let number, first, second, third, fourth, fifth
+
+number = prompt("Введите пятираздряное число")
+
+first = number.slice(0,1);
+second = number.slice(1,2);
+fourth = number.slice(3,4);
+fifth = number.slice(4);
+
+if ((first == fifth) && (second == fourth)) {
+    alert("Число является палиндромом");
+} else {
+    alert("Число не является палиндромом")
+}
+
+return false;
+}
